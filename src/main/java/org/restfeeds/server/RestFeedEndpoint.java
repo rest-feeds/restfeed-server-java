19729326850aa3462c07b465bfefb267bc5be730
Jochen Christ <jochen.christ@innoq.com>
2019-12-25 22:26:32 +0100

    Zero compile dependencies

--- src/main/java/org/restfeeds/server/RestFeedEndpoint.java
+++ src/main/java/org/restfeeds/server/RestFeedEndpoint.java
@@ -1,17 +1,17 @@
 package org.restfeeds.server;
 
+import static java.lang.String.format;
 import static java.time.temporal.ChronoUnit.MILLIS;
 import static java.time.temporal.ChronoUnit.SECONDS;
 
 import java.time.Duration;
 import java.time.Instant;
 import java.util.List;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.util.logging.Logger;
 
 public class RestFeedEndpoint {
 
-  static final Logger log = LoggerFactory.getLogger(RestFeedEndpoint.class);
+  private static final Logger log = Logger.getLogger(RestFeedEndpoint.class.getName());
 
   private final FeedItemRepository feedItemRepository;
   private final Duration pollInterval;
@@ -30,27 +30,28 @@ public class RestFeedEndpoint {
 
   public List<FeedItem> fetch(String feed, long offset, int limit) {
     Instant timeoutTimestamp = Instant.now().plus(timeout);
-    log.debug(
-        "Polling for items in feed {} with offset {} and a timeout of {}", feed, offset, timeout);
+    log.fine(
+        () -> format("Poll for items in feed %s with offset=%s timeout=%s", feed, offset, timeout));
     List<FeedItem> items;
     while (true) {
       items = feedItemRepository.findByFeedPositionGreaterThanEqual(feed, offset, limit);
 
-      if (items.size() > 0) {
-        log.debug("Returning {} items.", items.size());
+      int numberOfItems = items.size();
+      if (numberOfItems > 0) {
+        log.fine(() -> format("Returning %s items.", numberOfItems));
         return items;
       }
 
       if (Instant.now().isAfter(timeoutTimestamp)) {
-        log.debug("Polling timed out. Returning the empty response.");
+        log.fine("Polling timed out. Returning the empty response.");
         return items;
       }
 
       try {
-        log.trace("No items found. Wait a bit and then retry again.");
+        log.finest("No items found. Wait a bit and then retry again.");
         Thread.sleep(pollInterval.toMillis());
       } catch (InterruptedException e) {
-        log.debug("Thread was interrupted. Probably a graceful shutdown. Try to send response.");
+        log.fine("Thread was interrupted. Probably a graceful shutdown. Try to send response.");
         return items;
       }
     }
